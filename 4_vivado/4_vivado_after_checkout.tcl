06939de8ea618974dfb379f51cad9e1ee2fb72b2
lyrerwalter <151466441+lyrerwalter@users.noreply.github.com>
2023-12-06 20:04:07 +0100

    Correcttions

--- 4_vivado/4_vivado_after_checkout.tcl
+++ 4_vivado/4_vivado_after_checkout.tcl
@@ -3,7 +3,7 @@
 # – In Vivado navigate to the folder containing the TCL script
 # – Run the script:
 # cd <your-2_vivado-folder>
-source project.tcl
+source ./project.tcl
 
 
 # For each command, documentation is integrated into Vivado
